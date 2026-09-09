f3237448860097c3cf4b37d5176c07127d2d2a55
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-09 22:20:54 +0300

    feat: Adding Data Models
    Co-authored-by: AhmedAbdullah51219 <ahmednagezamalek@gmail.com>

---
+++ HotelListing.Api/Data/Hotel.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelListing.Api.Data;
+
+public class Hotel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public double Rating { get; set; }
+
+    public int CountryId { get; set; }
+    public Country Country { get; set; } = default!;
+
+}

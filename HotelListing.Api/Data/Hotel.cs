912238aece735fcc80dcfd06681a862b74553ec0
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-10 18:17:59 +0300

    feat:  Explore and Implement Validation Rules

--- HotelListing.Api/Data/Hotel.cs
+++ HotelListing.Api/Data/Hotel.cs
@@ -3,8 +3,8 @@
 public class Hotel
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Address { get; set; } = string.Empty;
+    public required string Name { get; set; } = string.Empty;
+    public required string Address { get; set; } = string.Empty;
     public double Rating { get; set; }
 
     public int CountryId { get; set; }

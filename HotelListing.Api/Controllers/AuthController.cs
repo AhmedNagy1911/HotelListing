3e1ac7c4ae2d55b132cf2af4374093063a0b023d
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-12 01:50:44 +0300

    feat: Add Custom Authentication Endpoints

---
+++ HotelListing.Api/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using HotelListing.Api.Constants;
+using HotelListing.Api.Data;
+using HotelListing.Api.DTOs.Auth;
+using HotelListing.Api.Results;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelListing.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController(UserManager<ApplicationUser> userManager) : BaseApiController
+{
+    private readonly UserManager<ApplicationUser> _userManager = userManager;
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterUserDto registerUserDto)
+    {
+        var user = new ApplicationUser
+        {
+            Email = registerUserDto.Email,
+            UserName = registerUserDto.Email,
+            FirstName = registerUserDto.FirstName,
+            LastName = registerUserDto.LastName
+        };
+          
+        var result = await _userManager.CreateAsync(user, registerUserDto.Password);
+
+        if (!result.Succeeded)
+        { 
+            var errors = result.Errors.Select(e =>new Error(ErrorCodes.BadRequest, e.Description)).ToArray();
+            
+            return MapErrorsToResponse(errors);
+        }
+
+
+
+        // Implementation for user registration
+        return Ok();
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginUserDto loginDto)
+    {
+        var user = await _userManager.FindByEmailAsync(loginDto.Email);
+        if(user == null)
+            return Unauthorized(new { message= "Invalid email or password."});
+
+        var isPasswordValid = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+        if (!isPasswordValid)
+            return Unauthorized(new { message= "Invalid email or password."});
+
+        // Implementation for user login
+        return Ok();
+    }
+}

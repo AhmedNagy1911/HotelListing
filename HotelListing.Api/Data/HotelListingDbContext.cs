0177c7e497178eaa3a67f38c5d73f67f381d3d5d
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-12 00:23:21 +0300

    feat: Customizing Identity Tables

--- HotelListing.Api/Data/HotelListingDbContext.cs
+++ HotelListing.Api/Data/HotelListingDbContext.cs
@@ -1,11 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelListing.Api.Data;
 
 public class HotelListingDbContext(DbContextOptions<HotelListingDbContext> options) 
-    :IdentityDbContext<IdentityUser>(options)
+    :IdentityDbContext<ApplicationUser>(options)
 {
     public DbSet<Country> Countries { get; set; }
     public DbSet<Hotel> Hotels { get; set; }

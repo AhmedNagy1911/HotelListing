40f5dd286f72e80919f30881d16785377f8e306e
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-13 01:44:14 +0300

    feat: Implementing Create Booking endpoints

---
+++ HotelListing.Api/Controllers/HotelBookingsController.cs
@@ -0,0 +1,28 @@
+﻿using HotelListing.Api.Contracts;
+using HotelListing.Api.DTOs.Booking;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelListing.Api.Controllers;
+
+[Route("api/hotels/{hotelId:int}/bookings")]
+[ApiController]
+[Authorize]
+public class HotelBookingsController(IBookingService bookingService) : BaseApiController
+{
+    private readonly IBookingService _bookingService = bookingService;
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<GetBookingDto>>> GetBookings([FromRoute] int hotelId)
+    {
+      //  var bookings = await _bookingService.GetBookingsByHotelIdAsync(hotelId);
+        return Ok();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<GetBookingDto>> CreateBooking([FromRoute] int hotelId,[FromBody] CreateBookingDto createBookingDto)
+    {
+        //  var booking = await _bookingService.CreateBookingAsync(hotelId, createBookingDto);
+        return CreatedAtAction(nameof(GetBookings), new { hotelId = hotelId }, null);
+    }
+}
